0400f256828a698263a84fa5c0b05aff21556a1d
anseane022 <anseane022@student.wethinkcode.co.za>
2024-11-04 15:20:19 +0200

    mongoDb connection

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('YOUR_MONGODB_CONNECTION_STRING', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
