0a0348125a0aa242cf23a68294845b00e047d386
anseane022 <anseane022@student.wethinkcode.co.za>
2024-11-05 17:52:18 +0200

    MongoDb fixes

--- backend/server.js
+++ backend/server.js
@@ -14,17 +14,9 @@ const corsOptions = {
 }; 
 
 app.use(cors(corsOptions));
-
-app.use(cors({ 
-  origin: '*', 
-}));
-
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 const taskSchema = new mongoose.Schema({
   name: String,
