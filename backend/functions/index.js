1c51f23c00eab15e1b62f8ffc3027d35325756e9
anseane022 <anseane022@student.wethinkcode.co.za>
2024-11-18 08:00:34 +0200

    importing and exporting tasks in index

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,5 +1,8 @@
 const functions = require("firebase-functions");
+const tasks = require("./tasks");
 
 exports.index = functions.https.onRequest((req, res) => {
   res.status(200).send("Backend is working!");
 });
+
+exports.api = tasks.api;
